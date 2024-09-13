30a7cf9a60395eb4e5755e27b208e567998ef0f8
Rachit Jain <rachitjain9596@gmail.com>
2024-09-13 18:22:03 +0530

    jwt setup

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register Route");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username & password", 400);
+  }
+
+  // just for demo, usually provided by DB!!!
+  const id = new Date().getDate();
+
+  // try to keep small, better UX
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
-    secret: `Authorized data, your lucky umber is: ${luckNumber}`,
+    secret: `Authorized data, your lucky umber is: ${luckyNumber}`,
   });
 };
 
