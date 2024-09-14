64ca5b4eb96519f708bef55f6ed597a89eb942d5
Rachit Jain <rachitjain9596@gmail.com>
2024-09-14 23:28:51 +0530

    errors-index

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custom-error");
+const BadRequest = require("./Bad-Request");
+const UnauthenticatedError = require("./unauthenticated");
+
+module.exports = {
+  CustomAPIError,
+  BadRequest,
+  UnauthenticatedError,
+};
